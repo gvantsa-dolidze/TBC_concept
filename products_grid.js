d890b67bbaf3ce396a99dffafd9badc935c7dfea
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-07-20 22:17:55 +0400

    position text on the grid images

--- products_grid.js
+++ products_grid.js
@@ -6,7 +6,11 @@ fetch("data.json")
       const gridElement = document.createElement("div");
       gridElement.className = `grid-item item${item.id}`;
       gridElement.innerHTML = `
-            <a href="#"> <img src="${item.img}"> </a>
+            <a href="#"> <img src="${item.img}"></a>
+            <div class="text_on_img">
+            <h3>${item.title}</h3>
+            <a href><span class="bi bi-arrow-right"></span><span>${item.link_name}</span></a>
+            </div>
 `;
       container.appendChild(gridElement);
     });

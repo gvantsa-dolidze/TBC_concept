9e9a500c05dc455fcbb3dd53396587691da05fdd
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-07-21 14:31:25 +0400

    update js file

--- awards_scrollbar.js
+++ awards_scrollbar.js
@@ -1,25 +1,25 @@
-fetch("data.json")
-  .then((response) => response.json())
-  .then((data) => {
-    const container = document.getElementById("awards_scrollbar");
-    data.awards.forEach((item) => {
-      const card = document.createElement("div");
-      card.className = "card";
-      card.innerHTML = `
-    <a href="#">
-    <div class="card_img_container">
-        <img src="${item.img}" class="bottom-img" alt="Image" width="50%">  
-        </div>
-      <div class="title_text">
-        <h3>${item.title}</h3>
-        <p>${item.paragraph}</p>
-      </div>
-    </a>
-`;
-      container.appendChild(card);
-    });
-  })
+// fetch("data.json")
+//   .then((response) => response.json())
+//   .then((data) => {
+// //     const container = document.getElementById("awards_scrollbar");
+// //     data.awards.forEach((item) => {
+// //       const card = document.createElement("div");
+// //       card.className = "card";
+// //       card.innerHTML = `
+// //     <a href="#">
+// //     <div class="card_img_container">
+// //         <img src="${item.img}" class="bottom-img" alt="Image" width="50%">  
+// //         </div>
+// //       <div class="title_text">
+// //         <h3>${item.title}</h3>
+// //         <p>${item.paragraph}</p>
+// //       </div>
+// //     </a>
+// // `;
+// //       container.appendChild(card);
+// //     });
+//   })
 
-  .catch((error) => {
-    console.error("Error fetching data:", error);
-  });
+//   .catch((error) => {
+//     console.error("Error fetching data:", error);
+//   });
--- offers_scrollbar.js
+++ offers_scrollbar.js
@@ -19,6 +19,42 @@ fetch("data.json")
 `;
       container.appendChild(card);
     });
+
+    const container3 = document.getElementById("products_scrollbar");
+    data.products.forEach((item) => {
+      const card = document.createElement("div");
+      card.className = "card";
+      card.innerHTML = `
+    <a href="">
+    <div class="">
+        <img src="${item.img}" class="bottom-img" alt="Image" width="100%">  
+        </div>
+      <div class="title_text">
+        <h3>${item.title}</h3>
+        <p>${item.paragraph}</p>
+      </div>
+    </a>
+`;
+      container3.appendChild(card);
+    });
+
+    const container1 = document.getElementById("awards_scrollbar");
+    data.awards.forEach((item) => {
+      const card = document.createElement("div");
+      card.className = "card";
+      card.innerHTML = `
+    <a href="#">
+    <div class="card_img_container">
+        <img src="${item.img}" class="bottom-img" alt="Image" width="50%">  
+        </div>
+      <div class="title_text">
+        <h3>${item.title}</h3>
+        <p>${item.paragraph}</p>
+      </div>
+    </a>
+`;
+      container1.appendChild(card);
+    });
   })
 
   .catch((error) => {
